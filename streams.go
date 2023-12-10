dc74e9b8f76d14a81a293427bb7e60d14ad1a5ed
Kelsey <12659696+KelseyAu@users.noreply.github.com>
2023-12-10 10:32:55 +0100

    feat: add support for multiple streams

---
+++ streams.go
@@ -0,0 +1,42 @@
+package wt
+
+import (
+	"log"
+)
+
+func (c *Connection) OpenStream() int64 {
+	str, err := c.Session.OpenStream()
+	if err != nil {
+		log.Println("Stream error: " + err.Error())
+	}
+	c.activeStream = str
+	c.streams[int64(str.StreamID())] = str
+
+	return int64(str.StreamID())
+}
+
+func (c *Connection) CloseStream() {
+	if c.activeStream != nil {
+		c.activeStream.Close()
+		delete(c.streams, int64(c.activeStream.StreamID()))
+		c.activeStream = nil
+	}
+}
+
+func (c *Connection) CloseStreamById(id int64) {
+	if c.streams[id] != nil {
+		c.streams[id].Close()
+		delete(c.streams, id)
+		// set stream?
+	}
+}
+
+func (c *Connection) CloseAllStreams() {
+	for id := range c.streams {
+		c.CloseStreamById(id)
+	}
+}
+
+func (c *Connection) SetActiveStream(id int64) {
+	c.activeStream = c.streams[id]
+}
