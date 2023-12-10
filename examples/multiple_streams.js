9b322bf77c0bf856b110310947943a196449eb70
Kelsey <12659696+KelseyAu@users.noreply.github.com>
2023-12-10 10:39:56 +0100

    chore: reset port

--- examples/multiple_streams.js
+++ examples/multiple_streams.js
@@ -1,7 +1,7 @@
 import wt from "k6/x/webtransport";
 
 export default function () {
-  const url = "https://localhost:8111/webtransport";
+  const url = "https://localhost:443/webtransport";
   let data = [0, 1, 2, 3, 4];
 
   wt.connect(url);
